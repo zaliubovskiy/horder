32e64c87f9abfbfefb788145e66edc8cabd40b6d
zaliubovskiy <maxim.zaliubovskiy@gmail.com>
2021-04-05 18:08:22 +0300

    Fixed-search-and-product-single-pages

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ProductView.as_view()),
+    path("", views.IndexView.as_view()),
+    path("<search>", views.ProductView.as_view()),
     path("<slug:slug>/", views.ProductDetailView.as_view(), name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -2,17 +2,34 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from .models import Product
+from .models import Product, Category, SubCategory
 
 
-class ProductView(ListView):
+class SubCategoryFilter:
+
+    def get_category(self):
+        return Category.objects.all()
+
+    def get_subcategory(self):
+        return SubCategory.objects.all()
+
+
+class ProductView(SubCategoryFilter, ListView):
     """list of models"""
     model = Product
     queryset = Product.objects.all()
-    template_name = "product_detail.html"
+    template_name = "search.html"
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(SubCategoryFilter, DetailView):
     """full description of the 3d Model"""
     model = Product
     slug_field = "url"
+    template_name = "product_detail.html"
+
+
+class IndexView(SubCategoryFilter, ListView):
+    """list of models"""
+    model = Product
+    queryset = Product.objects.all()
+    template_name = "index.html"
